79c814c6500dd0d5cd893c64c551112034e25cde
clairedeboer <claire.deboer@gmail.com>
2021-02-02 18:04:21 -0500

    fixed show/hide comments re-render

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,31 @@
-import React, { useState } from "react"; 
+import React, { useState } from "react";
 import video from "../data/video.js";
 
 function App() {
-  const [upVote, setUpVote] = useState(video.upvotes)
-  const [downVote, setDownVote] = useState(video.downvotes)
+  const [upVotes, setUpVotes] = useState(video.upvotes);
+  const [downVotes, setDownVotes] = useState(video.downvotes);
+  const [commentsHidden, setCommentsHidden] = useState(false);
 
-  const [hideComments, setHideComments] = useState(true);
-  
   const comments = video.comments.map((comment) => {
-    return <div key={comment.id}>
-      <strong>{comment.user}</strong>
-      <div>{comment.comment}</div>
-    </div>
-  })
+    return (
+      <div key={comment.id}>
+        <strong>{comment.user}</strong>
+        <div>{comment.comment}</div>
+      </div>
+    );
+  });
 
-  const handleUpVote = () => {
-    setUpVote((upVote) => upVote+1)
-  }
+  const handleUpVotes = () => {
+    setUpVotes((upVotes) => upVotes + 1);
+  };
 
-  const handleDownVote = () => {
-    setDownVote((downVote) => downVote-1)
-  }
+  const handleDownVotes = () => {
+    setDownVotes((downVotes) => downVotes - 1);
+  };
 
-  const handleHideComments = (event) => {
-    setHideComments((hideComments) => !hideComments);
-    const commentsDiv = document.querySelector(".comments")
-    if(commentsDiv.style.display === "none"){
-      commentsDiv.style.display = "block";
-      // event.target.textContent = 'Hide Comments'
-    }else if(commentsDiv.style.display === 'block'){
-      commentsDiv.style.display = "none";
-      // event.target.textContent = 'Show Comments'
-    }
-  }
+  const handleHideComments = () => {
+    setCommentsHidden((prevcommentsHidden) => !prevcommentsHidden);
+  };
 
   return (
     <div className="App">
@@ -49,15 +42,19 @@ function App() {
       <p>
         {video.views} Views | Uploaded {video.createdAt}
       </p>
-      <button onClick={handleUpVote}>{upVote}</button>
-      <button onClick={handleDownVote}>{downVote}</button>
+      <button onClick={handleUpVotes}>{upVotes}</button>
+      <button onClick={handleDownVotes}>{downVotes}</button>
       <div>
-      <button onClick={handleHideComments}>{hideComments ? "Hide Comments" : "Show Comments"}</button>
+        <button onClick={handleHideComments}>
+          {commentsHidden ? "Show Comments" : "Hide Comments"}
+        </button>
       </div>
-      <div className="comments">
-        <h4>{video.comments.length} Comments</h4>
-      </div>
-        {comments}
+      {!commentsHidden && (
+        <div className="comments">
+          <h4>{video.comments.length} Comments</h4>
+          {comments}
+        </div>
+      )}
     </div>
   );
 }
